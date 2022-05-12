17883a096ee35d862d780fc698a54d3fe9e1c62e
Kyle Sunderland <sunderlandkyl@gmail.com>
2022-05-12 17:56:38 -0400

    COMP: Lock IGSIO to specific commit

--- SuperBuild/External_IGSIO.cmake
+++ SuperBuild/External_IGSIO.cmake
@@ -24,7 +24,7 @@ if(NOT DEFINED ${proj}_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
 
   ExternalProject_SetIfNotDefined(
     ${CMAKE_PROJECT_NAME}_${proj}_GIT_TAG
-    "master"
+    "af2f49c0fe8e552ebbe12b165e71ea703a6e3d9d"
     QUIET
     )
 
